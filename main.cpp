6697001d7f465317df57de2894c9f29acbb2e1ca
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-05-17 00:57:51 +0900

    Add files to a new project.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	main.cpp
+ * @brief	selectable_stack.hppの動作確認用コマンド
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ * @note	see http://www.amazon.co.jp/dp/4839942390 .
+ */
+
+#include <cstddef>
+#include <cstdio>
+#include "selectable_stack.hpp"
+
+bool
+select(const int& left,
+	   const int& right)
+{
+	return left <= right;
+}
+
+/**
+ * 動作確認用コマンド
+ */
+int main()
+{
+	SelectableStack<int>* stack = new SelectableStack<int>(select);
+
+	stack->push(2);
+	stack->push(3);
+	stack->push(1);
+	stack->push(5);
+	stack->push(4);	// pop対象
+	stack->push(0);	// pop対象
+	stack->push(6);	// pop対象
+
+	stack->pop();
+	stack->pop();
+	stack->pop();
+
+	std::printf("last: %d\n", stack->top());
+	std::printf("min:  %d\n", stack->top(true));
+	std::printf("size: %lu\n", stack->size());
+
+	delete stack;
+
+	return 0;
+}
---
+++ selectable_stack.hpp
@@ -0,0 +1,125 @@
+/* -*- coding: utf-8; tab-width: 4 -*- */
+/**
+ * @file	selectable_stack.hpp
+ * @brief	最小値を最悪計算量Θ(1)で出力できるスタックの実装
+ * @author	Yasutaka SHINDOH / 新堂 安孝
+ */
+
+#include <cstddef>
+#include <cassert>
+#include <vector>
+
+/**
+ * @class	最小値を最悪計算量Θ(1)で出力できるスタック
+ * @note	テンプレートの型 @a TYPE はスタックのキーの型。
+ */
+template<typename TYPE>
+class SelectableStack
+{
+private:
+
+	std::vector<TYPE> data_;	///< 本来のスタック
+	std::vector<size_t> index_;	///< 最優先値のインデックスのスタック
+	size_t length_;				///< スタックの要素数
+
+	/**
+	 * 最優先を定義する関数
+	 * @note	第一引数を優先すべき時は @a true を、そうでないときは @a false を返す。
+	 */
+	bool (* select_)(const TYPE&, const TYPE&);
+
+public:
+
+	/**
+	 * コンストラクタ
+	 * @param[in]	select	最優先を定義する関数 (see @a select_ )
+	 */
+	SelectableStack(bool (* select)(const TYPE&, const TYPE&))
+		: length_(0),
+		  select_(select)
+		{
+			;
+		}
+
+	/**
+	 * スタックが空か確認
+	 * @return	true: 空, false: 空でない
+	 * @note	最悪計算量はO(1)。
+	 */
+	bool
+	empty() const
+		{
+			return length_ == 0;
+		}
+
+	/**
+	 * スタックの要素数を取得
+	 * @return	スタックの要素数
+	 * @note	最悪計算量はO(1)。
+	 */
+	size_t
+	size() const
+		{
+			return length_;
+		}
+
+	/**
+	 * スタックの値を取得
+	 * @param[in]	prior	返却値のフラグ
+	 * @return	最後にスタックに追加した値 (@a prior が @a false の時)
+				もしくは最優先値 (@a prior が @a true の時) を取得
+	 * @note	事前にスタックが空でないことを確認しておくこと。
+	 * @note	最悪計算量はO(1)。
+	 */
+	TYPE
+	top(bool prior = false) const
+		{
+			assert(0 < length_);
+
+			if (prior) {
+				size_t i = index_.back();
+				return data_[i];
+			}
+			else {
+				return data_.back();
+			}
+		}
+
+	/**
+	 * スタックに要素を追加
+	 * @param[in]	data	追加するデータ
+	 * @note	通常の std::vector であれば、ならし解析の計算量はO(1)となる。
+				最悪計算量はO(n)で、nはスタックの長さ。
+	 */
+	void
+	push(const TYPE& data)
+		{
+			assert(select_);
+
+			++length_;
+			data_.push_back(data);
+			if (1 < length_) {
+				size_t i = index_.back();
+				if (select_(data_[i], data)) {
+					index_.push_back(i);
+					return;
+				}
+			}
+			index_.push_back(length_ - 1);
+		}
+
+	/**
+	 * スタックから要素を削除
+	 * @note	事前にスタックが空でないことを確認しておくこと。
+	 * @note	最悪計算量はO(1)。
+	 */
+	void
+	pop()
+		{
+			assert(0 < length_);
+
+			--length_;
+			data_.pop_back();
+			index_.pop_back();
+		}
+};
